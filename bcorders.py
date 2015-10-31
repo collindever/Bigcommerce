d5e6f08610a7dde9cf6810b028d7d45c29cdea0a
Collin Dever <collindever@users.noreply.github.com>
2015-10-30 17:17:43 -0700

    Update bcorders.py
    This program pulls all of the order id's since it was last ran.  It confirms that orders have a captured payment because incomplete orders are listed in the xml. Then the script pulls the sku's and quantity of the order into a dictionary and if the same item is sold multiple times it adds the new quantity value to the existing sku dictionary entry

--- bcorders.py
+++ bcorders.py
@@ -9,29 +9,41 @@ target = open(filename, 'r')
 last_id = target.read()
 target.close()
 
-print last_id
-
 payload = {'min_id': last_id}
 
-print payload
-
-r = requests.get('STORE URL/api/v2/orders', params=payload, auth=(USER ID, API KEY))
+r = requests.get('STORE URL/api/v2/orders', params=payload, auth=('USER ID', 'API KEY'))
 
 tree = ET.fromstring(r.content)   
-print tree
 
 for order in tree.findall('order'):
 	payment_status = order.find('payment_status').text
 	if payment_status == 'captured':
 		order_id = order.find('id').text
-		print order_id
 		order_items.append( order_id )
-print  order_items
 
-#time = datetime.datetime.fromtimestamp(
-		#int(t)).strftime('%a%%2C%d+%b+%Y+%H%%3A%M%%3A%S+%%2B0000')
+max = len(order_items)
+cnt = 0
+order_inventory = {}
 
-#target = open(filename, 'w')
-#target.truncate()
-#target.write(time)
-#target.close
+while cnt < max:
+	r = requests.get('STORE URL/api/v2/orders/%s/products.xml' % order_items[cnt], auth=('USER ID', 'API KEY'))
+		
+	tree = ET.fromstring(r.content)
+	
+	for product in tree.findall('product'):
+		itmsku = product.find('sku').text
+		qty = product.find('quantity').text 
+		if itmsku in order_inventory:
+			order_inventory[itmsku] = order_inventory[itmsku] + qty	
+		else:
+			order_inventory[itmsku] = qty
+	cnt = cnt + 1
+
+cnt = cnt - 1
+newest_order = order_items[cnt]
+
+filename = 'lastupdate.txt'
+target = open(filename, 'w')
+target.truncate()
+target.write(newest_order)
+target.close
