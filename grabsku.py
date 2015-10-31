81a0f916d3d76332722e6a97e33bb815e5adfef3
Collin Dever <collindever@users.noreply.github.com>
2015-10-30 17:20:32 -0700

    Update grabsku.py

--- grabsku.py
+++ grabsku.py
@@ -1,13 +1,52 @@
 import requests
 import elementtree.ElementTree as ET
+import xml.parsers.expat
+import csv
 
-cnt = 1
-payload = {'limit': '250', 'page' : cnt}   #adds ?limit=250&page=cnt to the end of the GET request
-r = requests.get('https://STORE URL/api/v2/products/skus', params=payload, auth=('USER ID', 'API TOKEN'))  #get request pulls pages of 250 SKUS
+cnt =  1
+payload = {'limit': '200', 'page' : cnt}
+inventory = {}
 
-tree = ET.fromstring(r.content)   #imports the raw binary data from the site into Element Tree to read XML data
+while cnt != -1:
+	try:
+		r = requests.get('https://STORE URL/api/v2/products/skus', params=payload, auth=('USER ID', 'API KEY'))
 
-for sku in tree.findall('sku'):   
-	itmsku = sku.find('sku').text
-	qty = sku.find('inventory_level').text
-	print itmsku, qty
+		tree = ET.fromstring(r.content)   
+
+	except xml.parsers.expat.ExpatError, e:
+		cnt = -1
+
+	else:
+		for sku in tree.findall('sku'):
+			itmsku = sku.find('sku').text
+			qty = sku.find('inventory_level').text
+			inventory[itmsku] = qty
+
+		cnt = cnt + 1
+		payload = {'limit': '200', 'page' : cnt}
+
+	finally:
+		print len(inventory)
+
+cnt =  1
+payload = {'limit': '200', 'page' : cnt}
+
+while cnt != -1:
+	try:
+		r = requests.get('https://STORE URL/api/v2/products', params=payload, auth=('USER ID', 'API KEY'))
+
+		tree = ET.fromstring(r.content)   
+
+	except xml.parsers.expat.ExpatError, e:
+		cnt = -1
+
+	else:
+		for product in tree.findall('product'):
+			itmsku = product.find('sku').text
+			qty = product.find('inventory_level').text
+			inventory[itmsku] = qty
+
+		cnt = cnt + 1
+		payload = {'limit': '200', 'page' : cnt}
+	
+print len(inventory)
