fb7e7bb75222ec66712947a161949153a612615c
Collin Dever <collindever@users.noreply.github.com>
2016-01-20 14:03:18 -0800

    Update bcorders.py

--- bcorders.py
+++ bcorders.py
@@ -1,5 +1,3 @@
-import datetime
-import time
 import requests
 import elementtree.ElementTree as ET
 
@@ -9,9 +7,11 @@ target = open(filename, 'r')
 last_id = target.read()
 target.close()
 
-payload = {'min_id': last_id}
+payload = {'min_id': last_id, 'limit' : '250'}
 
-r = requests.get('STORE URL/api/v2/orders', params=payload, auth=('USER ID', 'API KEY'))
+print "Importing Orders Since Last Update"
+
+r = requests.get('https://<STORE URL>/api/v2/orders', params=payload, auth=(<USER ID>, <API KEY>))
 
 tree = ET.fromstring(r.content)   
 
@@ -26,21 +26,26 @@ cnt = 0
 order_inventory = {}
 
 while cnt < max:
-	r = requests.get('STORE URL/api/v2/orders/%s/products.xml' % order_items[cnt], auth=('USER ID', 'API KEY'))
+	r = requests.get('https://<STORE URL>/api/v2/orders/%s/products.xml' % order_items[cnt], auth=(<USER ID>, <API KEY>))
 		
 	tree = ET.fromstring(r.content)
 	
 	for product in tree.findall('product'):
-		itmsku = product.find('sku').text
-		qty = product.find('quantity').text 
-		if itmsku in order_inventory:
-			order_inventory[itmsku] = order_inventory[itmsku] + qty	
+		if (product.find('sku').text) is None:
+			cnt = cnt + 1
 		else:
-			order_inventory[itmsku] = qty
-	cnt = cnt + 1
+			itmsku = int(product.find('sku').text)
+			qty = int(product.find('quantity').text) 
+			if itmsku in order_inventory:
+				order_inventory[itmsku] = order_inventory[itmsku] + qty	
+			else:
+				order_inventory[itmsku] = qty
+		cnt = cnt + 1
 
 cnt = cnt - 1
 newest_order = order_items[cnt]
+
+print "Importing Orders Since Last Update COMPLETE"
 
 filename = 'lastupdate.txt'
 target = open(filename, 'w')
